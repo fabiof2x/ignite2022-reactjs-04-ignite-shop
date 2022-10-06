1d82468bf534f5b2a10d29cba013cd41f4539cd7
fabiof2x <fabioar@gmail.com>
2022-10-06 16:23:27 -0300

    Produto & Checkout - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat odio nemo commodi mollitia laborum, a nulla consequuntur hic ut impedit distinctio ex dolorum ullam voluptas tempore enim ad aliquid! Doloremque?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
