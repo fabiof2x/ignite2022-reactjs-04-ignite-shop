2e5137ab3842bdbc1e70a90b4a2ba38fcc7650a1
fabiof2x <fabioar@gmail.com>
2022-10-07 16:58:39 -0300

    Produto & Checkout - Fallback do SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/future/image"
+import { useRouter } from "next/router"
 import Stripe from "stripe"
 import { stripe } from "../../lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
@@ -15,6 +16,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <p>Loading...</p>
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -40,7 +47,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [
       { params: { id: 'prod_MZ54uJyDnuDLFx' } }
     ],
-    fallback: false
+    fallback: true,
+    // fallback: 'blocking',
   }
 }
 
